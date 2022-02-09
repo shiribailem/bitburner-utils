d6114d36027caf50f7af2f56b1a73e74f76e4c72
Solomon Bailem <chris@foggyminds.com>
2022-02-09 00:25:37 -0600

    Added crime data

---
+++ data/crimes.js
@@ -0,0 +1,19 @@
+/** @param {NS} ns **/
+export const crimes = [
+	"Shoplift",
+	"Rob store",
+	"Mug someone",
+	"Larceny",
+	"Deal Drugs",
+	"Bond Forgery",
+	"Traffick illegal Arms",
+	"Homicide",
+	"Grand theft Auto",
+	"Kidnap and Ransom",
+	"Assassinate",
+	"Heist"
+];
+
+export async function main(ns) {
+	ns.tprint("Contains a list of all crimes, use by importing `crimes`")
+}
