0316e1857a3f79511fa51585646e84b09bc48865
Solomon Bailem <chris@foggyminds.com>
2022-02-12 18:35:54 -0600

    Added underline, italics, and formatPrint

--- libraries/terminal.js
+++ libraries/terminal.js
@@ -9,7 +9,13 @@ const GUIDE=
 "    terminal.setColor(string/React Element, color): Returns a React Element wrapping the passed\n"+
 "             string or React Element to apply the color given.\n" +
 "    terminal.setBold(string/React Element): Like setColor, but sets bold.\n" +
-"    terminal.cwd(): Returns the current terminal directory."
+"    terminal.setUnderline(string/React Element): \n" +
+"    terminal.setItalics(string/React Element): \n" +
+"    terminal.cwd(): Returns the current terminal directory.\n" +
+"    terminal.formatPrint(format, text, ...): Takes format arguments and text in pairs.\n" +
+"            Formats: i=italics, b=bold, u=underline, or any color name. Can be separated with\n" +
+"                     ':' to use multiple.\n" +
+"            Example: terminal.formatPrint('b:i:white', 'this text is bold, italics, and white', 'b', 'this text is just bold')"
 
 // Terminal class to print to terminal with added features and without scriptname
 // prefix.
@@ -47,21 +53,53 @@ export class terminal {
 
 	// Add DIV tag with color css easily
 	setColor(text, color) {
-		return React.createElement("div", {style: {color: `${color}`}}, text);
+		return React.createElement("span", {style: {color: `${color}`}}, text);
 	}
 
-	// Add bold easily
+	// Functions to add basic formatting easily
 	setBold(text) {
+		return React.createElement("b", null, text)
+	}
+
+	setUnderline(text) {
 		return React.createElement("u", null, text)
+	}
+
+	setItalics(text) {
+		return React.createElement("i", null, text)
 	}
 
 	// Returns the current directory on the server
 	cwd() {
 		return this.term.cwd();
 	}
+
+
+	formatPrint() {
+		if (arguments.length % 2 != 0) throw("formatPrint arguments must come in pairs. Format,Text");
+		let out = [];
+		for (let i=0;i<arguments.length;i+=2) {
+			let control = arguments[i].split(":");
+			let item = arguments[i+1];
+			for (let x=0; x < control.length;x++) {
+				if (control[x] == 'bold' || control[x] == 'b') {
+					item = this.setBold(item);
+				} else if (control[x] == 'italics' || control[x] == 'i') {
+					item = this.setItalics(item);
+				} else if (control[x] == 'underline' || control[x] == 'u') {
+					item = this.setUnderline(item);
+				} else {
+					item = this.setColor(item, control[x])
+				}
+			}
+			out.push(item);
+		}
+
+		this.fancyPrint(out);
+	}
 }
 
 export async function main(ns) {
 	const term = new terminal();
-	term.print(GUIDE)
+	term.print(GUIDE);
 }
